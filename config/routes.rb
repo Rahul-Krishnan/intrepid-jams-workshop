3f75d6419cce1c24bf8e7f2009c0640f5e1f8bc8
Rahul Krishnan <rk2211@gmail.com>
2017-08-17 01:29:56 -0400

    Added individual team endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # get '/games' => 'games#index'
   resources :games, only: [:index]
-  resources :teams, only: [:index]
+  resources :teams, only: [:index, :show]
 end
