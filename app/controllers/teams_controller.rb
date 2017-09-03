e8ca54b88b2c667f035bf71549521a72d67b607b
Rahul Krishnan <rk2211@gmail.com>
2017-09-03 18:15:31 -0400

    added status

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,11 @@
 class TeamsController < ApplicationController
   def index
     teams = Team.all
-    render json: teams
+    render json: teams, status: 200
   end
 
   def show
     team = Team.find(params[:id])
-    render json: team
+    render json: team, status: 200
   end
 end
